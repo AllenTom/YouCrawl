0b2f5ed9b35f40d1791a4642d45d2e7d1985798f
allentom <qweasdzxczse2@gmail.com>
2020-09-20 21:35:49 +0900

    update:add custom json data post process

--- json.go
+++ json.go
@@ -7,10 +7,14 @@ import (
 
 type OutputJsonPostProcess struct {
 	StorePath string
+	GetData   func(store GlobalStore) interface{}
 }
 
 func (p *OutputJsonPostProcess) Process(store GlobalStore) error {
 	data := store.GetValue("items")
+	if p.GetData != nil {
+		data = p.GetData(store)
+	}
 	file, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
 		return err
